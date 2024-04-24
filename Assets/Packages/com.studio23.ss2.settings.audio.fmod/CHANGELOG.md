434ddde70d988fb0df8e8a20b0061841be04f09e
dipokDipu <90243333+dipokDipu@users.noreply.github.com>
2024-04-24 16:13:45 +0600

    Removed Resource Folder

--- Assets/Packages/com.studio23.ss2.settings.audio.fmod/CHANGELOG.md
+++ Assets/Packages/com.studio23.ss2.settings.audio.fmod/CHANGELOG.md
@@ -3,6 +3,13 @@
 All notable changes to this Unity package will be documented in this file.
 
 
+## [v1.0.1] - 2024-04-24
+
+### Added
+
++ Removed Resources from runtime folder
++ Made chnages so that settings work with new version of FMOD manager
+
 
 ## [v1.0.0] - 2024-03-25
 
---
+++ Assets/Packages/com.studio23.ss2.settings.audio.fmod/Samples/Demo/Scripts/Resources/FMOD_Data/FMODVCAList.cs
@@ -0,0 +1,16 @@
+
+namespace com.studio23.ss2.settings.audio.fmod.Sample
+{
+	public static class FMODVCAList
+	{
+		public static readonly string SFX = "vca:/SFX";
+		public static readonly string VO = "vca:/VO";
+		public static readonly string Master = "vca:/Master";
+		public static readonly string UI = "vca:/UI";
+		public static readonly string Environment = "vca:/Environment";
+		public static readonly string Sample = "vca:/Sample";
+		public static readonly string Music = "vca:/Music";
+		public static readonly string Player = "vca:/Player";
+		public static readonly string Equipment = "vca:/Equipment";
+	}
+}
